17a647a225440652bfd493977804d66cfb323112
Peter Kunszt <pkunszt@McPeter.home>
2020-03-08 16:43:45 +0100

    adding tests, changing for path only in the object

--- image_list.py
+++ image_list.py
@@ -13,6 +13,21 @@ class ImageList:
         self.invalid_types_found = set()
 
     def scan_directory(self, directory_name):
+        """Scanning a given directory for image and video files.
+        Returns a list of dictionaries with an entry for each
+        image or video file, containing:
+
+        name -- the name of the file,
+        path -- the full path to the file,
+        size -- its size in bytes,
+        created -- its created date in seconds,
+        checksum -- its checksum using blake2b hash
+        type -- basically the file ending
+        kind -- 0 is image, 1 is video
+
+        Argument:
+        directory_name -- name of the full path of the directory to scan.
+        """
         mode: int = os.stat(directory_name).st_mode
 
         # check that the name given is indeed a directory
@@ -29,7 +44,7 @@ class ImageList:
                     if image_kind < 0:
                         continue
                     file_item = dict(name=item.name,
-                                     path=item.path,
+                                     path=self.get_path_only(item.path),
                                      size=st.st_size,
                                      created=st.st_birthtime,
                                      checksum=self.checksum(item.path),
@@ -41,6 +56,7 @@ class ImageList:
 
     @staticmethod
     def checksum(filename):
+        """Return the hex representation of the checksum on the full binary using blake2b. Needs full path."""
         file_hash = hashlib.blake2b()
         with open(filename, "rb") as f:
             for chunk in iter(lambda: f.read(8192), b""):
@@ -48,6 +64,7 @@ class ImageList:
         return file_hash.hexdigest()
 
     def get_kind(self, image_type):
+        """Depending on known extensions, return 0 for images, 1 for videos and -1 for unknown"""
         if image_type in self.image_types:
             return 0
         if image_type in self.video_types:
@@ -57,6 +74,7 @@ class ImageList:
 
     @staticmethod
     def get_file_type(filename):
+        """Given a file name return the extension as its type."""
         extension_start = filename.rfind('.')
         if extension_start != -1:
             file_type = filename[extension_start + 1:].lower()
@@ -66,7 +84,17 @@ class ImageList:
         return file_type
 
     def get_invalid_types_found(self):
+        """Return the set of invalid types found, ie files that have not been processed"""
         return self.invalid_types_found
 
     def get_file_list(self):
+        """Return the file list, same as return value from scan_directory"""
         return self.file_list
+
+    @staticmethod
+    def get_path_only(file_name):
+        """Just keep the path of the file name, dis"""
+        last_slash = file_name.rfind('/')
+        if last_slash < 0:
+            return ""
+        return file_name[0:last_slash]
--- test_image_list.py
+++ test_image_list.py
@@ -27,11 +27,32 @@ class TestImageList(TestCase):
 
     def test_scan(self):
         from image_list import ImageList
+        files = ["cartoon.png", "spiderman.jpg", "spidey.jpeg", "thor.jpeg"]
+        sizes = [876273, 28398, 28398, 5906]
+        types = ["png", "jpg", "jpeg", "jpeg"]
+        kinds = [0, 0, 0, 0]
         test_list = ImageList()
 
         file_list = test_list.scan_directory("./testfiles")
-        names = []
-        for item in file_list:
-            names.append(item["name"])
-        self.assertTrue("spiderman.jpg" in names)
-        self.assertTrue("thor.jpeg" in names)
+        sorted_list = sorted(file_list, key=lambda i: i['name'])
+
+        for i in range(0, len(file_list)):
+            self.assertEqual(files[i], sorted_list[i]['name'])
+            self.assertEqual(sizes[i], sorted_list[i]['size'])
+            self.assertEqual(types[i], sorted_list[i]['type'])
+            self.assertEqual(kinds[i], sorted_list[i]['kind'])
+
+    def test_scan_invalid_input(self):
+        from image_list import ImageList
+        test_list = ImageList()
+
+        self.assertRaises(FileNotFoundError, test_list.scan_directory, "blah")
+        self.assertRaises(NotADirectoryError, test_list.scan_directory, "./test_image_list.py")
+
+    def test_get_path_only(self):
+        from image_list import ImageList
+        test_list = ImageList()
+
+        self.assertEqual(test_list.get_path_only("one/two/three.four"), "one/two")
+        self.assertEqual(test_list.get_path_only("./file.txt"), ".")
+        self.assertEqual(test_list.get_path_only("file.txt"), "")
