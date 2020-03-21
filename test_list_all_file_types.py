65453561669c7ebc4d12567397f0a084613a46db
Peter Kunszt <pkunszt@McPeter.home>
2020-03-21 14:53:32 +0100

    Add new file for listing all file types recursively in a directory. Add a few more verbose tests

---
+++ test_list_all_file_types.py
@@ -0,0 +1,16 @@
+from unittest import TestCase
+
+
+class TestListAllFileTypes(TestCase):
+    def test_scan(self):
+        from list_all_file_types import ListAllFileTypes
+        test_directory_list = ListAllFileTypes("./testfiles")
+        print(test_directory_list.get_list_of_found_types())
+        self.assertEquals(test_directory_list.get_list_of_found_types(), {"jpg", "png", "jpeg"})
+
+    def test_scan_invalid_directory_to_scan(self):
+        from list_all_file_types import ListAllFileTypes
+        test_directory = ListAllFileTypes()
+
+        self.assertRaises(FileNotFoundError, test_directory.scan, "blah")
+        self.assertRaises(NotADirectoryError, test_directory.scan, "./test_images_in_directory.py")
