51b7c566a21047be9d606f37d804dba63accb80b
Peter Kunszt <pkunszt@McPeter.home>
2020-04-13 13:15:11 +0200

    Factory should raise FactoryError

--- data/factory.py
+++ data/factory.py
@@ -20,17 +20,17 @@ class Factory:
         elif e.kind == 1:
             item = Video()
         else:
-            raise InvalidImageError(f"Entry mismatch, wrong kind {e.kind} found for: {e.name}; id:{e.meta.id}")
+            raise FactoryError(f"Entry mismatch, wrong kind {e.kind} found for: {e.name}; id:{e.meta.id}")
 
         item.name = e.name
         if e.type != item.type:
-            raise InvalidImageError(f"Type mismatch for {e.name}")
+            raise FactoryError(f"Type mismatch for {e.name}")
         item.path = e.path
         item.size = e.size
         item.checksum = e.checksum
         item.date = e.created
         if item.hash != e.hash:
-            raise InvalidImageError(f"Hash mismatch for {e.name}")
+            raise FactoryError(f"Hash mismatch for {e.name}")
         item.id = e.meta.id
 
         return item
